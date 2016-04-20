b959b0d90af0e014fe1759928a6ab0518b97266b
curogihu <curoas@yahoo.co.jp>
2016-04-20 12:37:38 -0700

    fix mismatch of header and footer

--- PortfolioSiteLaravel/resources/views/layouts/footer.blade.php
+++ PortfolioSiteLaravel/resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <div id="footer">
   <a href="#top" class="scrollLink">^</a>
-  <p>&copy; 2016 Being a backend developer, Yony in Vancouver</p>
+  <p>&copy; 2016 Being a needed developer, Yony in Vancouver</p>
 </div>
